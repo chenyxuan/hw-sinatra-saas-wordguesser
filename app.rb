498f86748930e121f89fece7fd61b6863d229f75
Yuxuan Chen <chenyxuan@sjtu.edu.cn>
2021-10-19 08:14:42 +0000

    Ready for Heroku!

--- app.rb
+++ app.rb
@@ -40,6 +40,15 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+
+    begin
+      valid = @game.guess(letter)
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    else
+      if not valid
+        flash[:message] = "You have already used that letter."
+    end
     redirect '/show'
   end
   
--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -7,8 +7,66 @@ class WordGuesserGame
 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
+  def word
+    @word
+  end
+
+  def guesses
+    @guesses
+  end
+
+  def wrong_guesses
+    @wrong_guesses
+  end
+
+  def guess(gword)
+    if not (gword =~ /[a-zA-Z]/)
+      raise ArgumentError
+    end
+    gword = gword.downcase
+    @word.chars do |letter|
+      if letter == gword
+        if @guesses.include? letter
+          return false
+        else
+          @guesses << letter
+          return true 
+        end
+      end
+    end
+    if @wrong_guesses.include? gword
+      return false
+    else
+      @wrong_guesses << gword
+      return true
+    end
+  end
+
+  def word_with_guesses
+    result = ''
+    @word.chars do |letter|
+      if @guesses.include? letter
+        result << letter
+      else 
+        result << '-'
+      end
+    end
+    result
+  end
+
+  def check_win_or_lose
+    if self.word_with_guesses == @word
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
+  end
   # You can test it by installing irb via $ gem install irb
   # and then running $ irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> WordGuesserGame.get_random_word
